0764c6b7e0882fb14203c590ab24616d8a8ec0c2
liliana oliveira <sliliana.oliveira@gmail.com>
2025-01-22 11:14:28 +0000

    Bibliotecas datetime math e random

---
+++ FT03/ex2.py
@@ -0,0 +1,6 @@
+numero1 = float(input("Digita o primeiro número: "))
+numero2 = float(input("Digita o segundo número: "))
+if numero1 == numero2:
+    print("Os números são iguais.")
+else:
+    print("Os números são diferentes.")
